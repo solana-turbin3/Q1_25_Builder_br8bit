78ddad3a4e10cdc4bce86d34424dc45313afae23
br8bit <>
2025-03-06 13:27:30 +0400

    move to a standalone repo

--- turbin3-prereq-ts/keygen.ts
+++
@@ -1,8 +0,0 @@
-import { Keypair } from "@solana/web3.js";
-
-const keypair = Keypair.generate();
-console.log(`You have generated a new Solana wallet: ${keypair.publicKey.toBase58()}
-  
-To save your wallet, copy and paste the following into a JSON file:
-
-[${keypair.secretKey}]`);
